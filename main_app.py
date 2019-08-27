c614cf645ba18f628527842f7cf515d740aafbb1
dipikabaad <dipika.baad@gmail.com>
2019-08-27 13:16:02 +0000

    Objects detected output added

--- main_app.py
+++ main_app.py
@@ -4,10 +4,10 @@ from database_operations import add_model_result
 
 app = Flask(__name__)
 
-@app.route("/", methods=['POST'])
+@app.route("/")
 def home():
-    req_data = request.get_json()
-    print(req_data)
+    # req_data = request.get_json()
+    # print(req_data)
     message, status = add_model_result({'name': 'dipika', 'val': 'new'})
     return jsonify({"message": message, "status": str(status) })
     # return "Hello, World!"
